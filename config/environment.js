0b4a530675077d7dd10cc56d9b9b625be546d9a8
Taras Kiricenko <taras.kiricenko@gmail.com>
2021-08-07 14:14:53 +0300

    map issues sorted

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,12 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/workspaceapi' //! get the name for database after last slash // 1.1
+// export const port = 4000
+// export const dbURI = 'mongodb://localhost/workspaceapi' //! get the name for database after last slash // 1.1
 
-//! 19.0
-export const secret = 'our tiny smallest ever secret'
+// //! 19.0
+// export const secret = 'our tiny smallest ever secret'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/workspaceapi/'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
